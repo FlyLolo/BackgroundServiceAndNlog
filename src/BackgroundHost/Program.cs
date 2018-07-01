11398eb5c5e35782895eb12b918bac0b01ba6da4
FlyLolo <FlyLolo@126.com>
2018-07-01 17:05:56 +0800

    first

---
+++ src/BackgroundHost/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+
+namespace BackgroundHost
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            NLog.Web.NLogBuilder.ConfigureNLog("nlog1.config");  //假如没有用默认的名字，多写了一个1
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders(); //移除已经注册的其他日志处理程序
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace); //设置最小的日志级别
+                })
+                .UseNLog();  
+    }
+}
---
+++ src/BackgroundHost/TokenRefreshService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FL.BackgroundHost
+{
+    internal class TokenRefreshService : BackgroundService
+    {
+        private readonly ILogger _logger;
+
+        public TokenRefreshService(ILogger<TokenRefreshService> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Service starting");
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation(DateTime.Now.ToLongTimeString() + ": Refresh Token!");
+                await Task.Delay(15000, stoppingToken);
+            }
+
+            _logger.LogInformation("Service stopping");
+        }
+    }
+}
